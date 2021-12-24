bf9a9bb3fdeefc3687fa40683636b5712e02a8ce
taisei <b1916560@planet.kanazawa-it.ac.jp>
2021-12-24 14:13:02 +0900

    make it better

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
+from linebot.models import     (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
